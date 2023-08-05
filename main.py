3b68e10c6b260bc824c438bfe542e767dd822321
iron-blooded <ayss2@mail.ru>
2023-08-06 01:18:10 +1200

    добавил анкеты

--- main.py
+++ main.py
@@ -26,15 +26,24 @@ from time import sleep
 
 
 class config:
-    server_kuratorka = 1137004117647171614  # id сервера кураторки
-    message_reacting = 1137019048077557781  # id сообщения, под которое люди должны поставить реакцию якобы для прохождения кураторки
-    channel_alert = (
-        1137016771455488060  # id канала, в которое бот будет срать оповещениями
-    )
-    server_HG = 612339223294640128  # id сервера HG
-    role_wait_kurator = 1137020285405630517  # роль ожидание кураторки
-    role_vereficate = 1137018790035599501  # роль верефицирован
-    role_participant = 612341683014598656  # роль участник
+    server_kuratorka = 1137004117647171614 
+    '''id сервера кураторки'''
+    message_reacting = 1137019048077557781
+    '''id сообщения, под которое люди должны поставить реакцию якобы для прохождения кураторки'''
+    channel_alert = 1137016771455488060
+    '''id канала, в которое бот будет срать оповещениями'''
+    server_HG = 612339223294640128
+    """id сервера HG"""
+    role_wait_kurator = 1137020285405630517
+    """роль ожидание кураторки"""
+    role_vereficate = 1137018790035599501
+    """роль верефицирован"""
+    role_participant = 612341683014598656
+    """роль участник"""
+    channel_writing_anketa = 1137358502239682712
+    """канал с написанием анкет"""
+    role_curator = 1137018745567584326
+    """куратор"""
 
     def __init__(self) -> None:
         pass
@@ -92,20 +101,28 @@ async def on_member_join(member: discord.Member):
 
 
 @client.event
+async def on_message(message: discord.Message):
+    if message.channel.id == config.channel_writing_anketa:
+        await message.add_reaction("✅")
+        await message.add_reaction("❌")
+        return
+
+
+@client.event
 async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
-    user = discord.utils.get(client.get_all_members(), id=payload.user_id)
+    user = payload.member
     if user.id == client.user.id:
         return
     emoji = payload.emoji
+    message_reacted = await client.get_channel(payload.channel_id).fetch_message(
+        payload.message_id
+    )
     if (
         payload.message_id == config.message_reacting
     ):  # если реакцию поставили под сообщение с запросом кураторки
         await user.add_roles(
             get_role(config.server_kuratorka, config.role_wait_kurator),
             reason="Запросил кураторку",
-        )
-        message_reacted = await client.get_channel(payload.channel_id).fetch_message(
-            payload.message_id
         )
         await message_reacted.remove_reaction(payload.emoji, user)
         channel = client.get_channel(config.channel_alert)
@@ -119,9 +136,6 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
     if (
         payload.channel_id == config.channel_alert
     ):  # если канал - канал с алертами кураторов
-        message_reacted = await client.get_channel(payload.channel_id).fetch_message(
-            payload.message_id
-        )
         user_wait_kuratorka = None
         for user_reacted in message_reacted.mentions:
             user_wait_kuratorka = user_reacted
@@ -146,6 +160,27 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
             )
         await message_reacted.clear_reactions()
         return
+    if (
+        payload.channel_id == config.channel_writing_anketa
+    ):  # если канал - канал с анкетами
+        if config.role_curator not in [i.id for i in user.roles]:
+            await message_reacted.remove_reaction(payload.emoji, user)
+            return
+        if emoji.name == "✅":
+            channel = client.get_channel(config.channel_alert)
+            message = await channel.send(
+                f"""✅ Пользователь <@{message_reacted.author.id}> успешно прошел кураторку написав анкету.\nПодтвердил: <@{user.id}>""",
+                tts=True,
+            )
+            await message_reacted.author.remove_roles(
+                get_role(config.server_kuratorka, config.role_wait_kurator),
+                reason="Анкета принята",
+            )
+            await message_reacted.author.add_roles(
+                get_role(config.server_kuratorka, config.role_vereficate),
+                reason="Анкета принята",
+            )
+            return
 
 
 async def play_music(voice_channel: discord.VoiceChannel):
@@ -156,23 +191,26 @@ async def play_music(voice_channel: discord.VoiceChannel):
             if os.path.isfile(file_path):
                 file_list.append(filename)
         return file_list
+
     voice_client = await voice_channel.connect()
     try:
         while voice_client.is_connected:
             music = get_files_in_directory("music")
             random.shuffle(music)
-            await voice_client.play(discord.FFmpegPCMAudio("music/"+music[-1]), after=lambda e: voice_client.disconnect())
+            await voice_client.play(
+                discord.FFmpegPCMAudio("music/" + music[-1]),
+                after=lambda e: voice_client.disconnect(),
+            )
     except Exception as e:
         print(e)
     # await voice_client.disconnect()
-
 
 
 @client.event
 async def on_voice_state_update(
     member: discord.Member, before: discord.VoiceState, after: discord.VoiceState
 ):
-    if member.bot:
+    if member.bot or member.guild == config.server_HG:
         return
     if before.channel != after.channel:
         voice_channel = after.channel
